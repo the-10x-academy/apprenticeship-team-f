cbf4682898993ff04f02d0842cc088bd88994668
Aju <aju0ajmal0k@gmail.com>
2021-02-02 23:38:05 +0530

    creating new uploadPage

--- client/src/Components/HomePage/postDetails/postDetails.js
+++ client/src/Components/HomePage/postDetails/postDetails.js
@@ -1,56 +1,52 @@
 import React from "react";
-import "./PostDetails.css";
+import "./postDetails.css";
 import more_icon from "./icons/more_icon.svg";
 import likes_icon from "./icons/heart.png";
 import share_icon from "./icons/share.png";
 
-
 export default function postDetails() {
-  return (
-    <div className="page">
-    <div className="App">  
+	return (
+		<div className="page">
+			<div className="App">
+				<div className="head_section">
+					<div className="post_details_section">
+						<p className="name"> Siva </p>
 
-      <div className="head_section">
+						<p className="place"> Bengaluru </p>
+					</div>
 
-          <div>
-           <p  className="name">        Siva         </p>
-          </div>
-          
-          <div className="more_icon_class">
-            <img className="more_icon"   alt=""       src={more_icon} />
-          </div>
+					<div className="more_icon_class">
+						<img className="more_icon" alt="" src={more_icon} />
+					</div>
+				</div>
 
-          <div>
-            <p  className="place">       Bengaluru    </p>
-          </div>  
+				<div className="avatar_class">
+					<img
+						className="avatar"
+						alt=""
+						src={
+							"https://th.bing.com/th/id/Rd58339de61b5d07fd4db988f8e8a7cdd?rik=1%2fUoNcqqIfTiXw&riu=http%3a%2f%2fthewowstyle.com%2fwp-content%2fuploads%2f2015%2f01%2fnature-images..jpg&ehk=%2fG27fwqbQI%2fi%2bxgGclM1BHuObngpvsp27tc36F59T9c%3d&risl=&pid=ImgRaw"
+						}
+					/>
+				</div>
 
-      </div>  
+				<div className="likes_share_date">
+					<div className="likes_share">
+						<img className="likes_icon" alt="" src={likes_icon} />
+						<img className="share_icon" alt="" src={share_icon} />
+					</div>
 
-      <div className="avatar_class">
-        <img  className="avatar"     alt=""       src={"https://th.bing.com/th/id/Rd58339de61b5d07fd4db988f8e8a7cdd?rik=1%2fUoNcqqIfTiXw&riu=http%3a%2f%2fthewowstyle.com%2fwp-content%2fuploads%2f2015%2f01%2fnature-images..jpg&ehk=%2fG27fwqbQI%2fi%2bxgGclM1BHuObngpvsp27tc36F59T9c%3d&risl=&pid=ImgRaw"} />
-      </div>
-      
-      <div className="likes_share_date">
+					<div className="date">
+						<h4> 10 Jan 2021 </h4>
+					</div>
+				</div>
 
-        <div  className="likes_share">
-          <img className="likes_icon"   alt=""       src={likes_icon} />
-          <img className="share_icon"   alt=""       src={share_icon} />
-        </div>
-
-        <div className="date">
-          <h4>  10 Jan 2021  </h4>
-        </div>
-
-      </div>  
-
-      <h4  className="likes">       64 likes     </h4>
-      <h3  className="description"> Kick start your career with a bang</h3>
-
-    </div>
-    </div>
-  );
-}  
-
+				<h4 className="likes"> 64 likes </h4>
+				<h3 className="description"> Kick start your career with a bang</h3>
+			</div>
+		</div>
+	);
+}
 
 // export default function postDetails(props) {
 //   return (
@@ -66,13 +62,4 @@ export default function postDetails() {
 //       <h3  className="description"> {props.description}  </h3>
 //     </div>
 //   );
-// }  
-
-
-
-
-
-
-
-
-
+// }
