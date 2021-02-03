4b2df789a4e9d7f0274fadeb09736d9a31d7abe9
Alekya-GitHubb <77324487+Alekya-GitHubb@users.noreply.github.com>
2021-02-03 08:43:24 +0530

    Update login.js

--- Models/login.js
+++ Models/login.js
@@ -1,12 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const login_schema = mongoose.Schema({
-// 	username: { type: String, required: true },
-// 	location: { type: String, required: true },
-// });
-
-// module.exports = mongoose.model("login", login_schema); /// login collections created
-
 const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema({
 	username: {
@@ -20,14 +11,14 @@ const postSchema = new mongoose.Schema({
 	},
 
 	comment: {
-		////newly added fn
+		
 		type: String,
 		required: true,
 	},
-	///need to add image route and likes
+	
 
 	likes: {
-		////newly added fn
+		
 		type: Number,
 		required: true,
 	},
@@ -37,6 +28,5 @@ const postSchema = new mongoose.Schema({
 		default: Date.now,
 	},
 });
-//collection creation
-// const Post = new mongoose.model("Post",postSchema);
+
 module.exports = mongoose.model("Post", postSchema);
