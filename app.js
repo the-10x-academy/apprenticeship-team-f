f8d92478a4b6b0fdfc1990651075e6a8c5bee1e6
Alekya-GitHubb <77324487+Alekya-GitHubb@users.noreply.github.com>
2021-02-05 19:37:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const bodyParser = require("body-parser"); 
 const indexRouter = require("./routes/index");
 
 const app = express();
@@ -20,8 +19,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true })); 
 
 
 app.use(function (req, res, next) {
