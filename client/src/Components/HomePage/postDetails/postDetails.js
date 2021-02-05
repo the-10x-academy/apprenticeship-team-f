2798764c772bcfe63fa02cea76e724cb41c7babf
Aju <aju0ajmal0k@gmail.com>
2021-02-05 19:35:46 +0530

    css upadted

--- client/src/Components/HomePage/postDetails/postDetails.js
+++ client/src/Components/HomePage/postDetails/postDetails.js
@@ -12,46 +12,44 @@ export default function postDetails(props) {
 
 
         <div className="nameLocation">
-
           <h1  className="name">        {props.name}  			 </h1>
           <h2  className="location">    {props.location}     </h2>
-
         </div>
-        <div className="moreIconClass">
 
+        <div className="moreIconClass">
           <img className="moreIcon"     alt=""               src={more_icon} /> 
-        
         </div>  
 
+
       </div>  
+
+
       <div className="midSection">  
 
        
         <img className="avatar"         alt=""               src={props.image} />
-      
-      
+    
+
       </div> 
+
+
       <div className="footSection">
       
       
         <div className="likesShareDate">
-        
           <div className="likesShare">
             <img className="likesIcon"  alt=""               src={likes_icon} />
             <img className="shareIcon"  alt=""               src={share_icon} />
           </div>
-          <h6  className="date">        {props.date}        	</h6>
-        
+          <h6  className="date">        {props.date}         </h6>
         </div>
+
         <div>
-        
           <h6  className="likes">       {props.likes}        </h6>
-        
         </div> 
+
         <div>
-        
           <h3  className="description"> {props.description}  </h3>
-        
         </div> 
       
       
