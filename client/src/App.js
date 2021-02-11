b3295d31807cb7022142bdbb78eff39dfe9d2bd2
Dinesh Bhadane <61912193+Dinesh3098@users.noreply.github.com>
2021-02-11 23:13:25 +0530

    For review upload image using multer (#14)
    * Adding Updated Homepage(landing page) with resolved changes
    
    * For review upload image using multer

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { 
-        BrowserRouter as Router,
-        Route,Link,Switch,Redirect
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+	Switch,
+	Redirect,
 } from "react-router-dom";
 
 import HomePage from "./Components/HomePage/HomePage";
 import PostList from "./Components/HomePage/PostList.js";
 
 function App() {
-    return (
-            <Router>
-                <switch>
-                    <Route exact path="/" component={HomePage} />
-                    <Route exact path="/insta" component={PostList} />
-                </switch>
-                
-            </Router>
-    );
+	return (
+		<Router>
+			<switch>
+				<Route exact path="/" component={HomePage} />
+				<Route exact path="/insta" component={PostList} />
+			</switch>
+		</Router>
+	);
 }
 
-export default App;  
-
-
+export default App;
--- client/src/Components/HomePage/postDetails/postDetails.js
+++ client/src/Components/HomePage/postDetails/postDetails.js
@@ -5,51 +5,44 @@ import likes_icon from "./icons/heart.png";
 import share_icon from "./icons/share.png";
 
 export default function postDetails(props) {
-  // const imagePath = "./../../../../../" + props.image ;
+	// const imagePath = "./../../../../../" + props.image ;
 
-  return (
-    <div className="Page">
-    <div className="App">
-      <div className="headSection">
-        <div className="nameLocation">
-          <h1  className="name">        {props.name}  			 </h1>
-          <h2  className="location">    {props.location}     </h2>
-        </div>
+	return (
+		<div className="Page">
+			<div className="App">
+				<div className="headSection">
+					<div className="nameLocation">
+						<h1 className="name"> {props.name} </h1>
+						<h2 className="location"> {props.location} </h2>
+					</div>
 
-       
-        <img className="moreIcon"     alt=""               src={more_icon} /> 
-    
-      </div>  
+					<img className="moreIcon" alt="" src={more_icon} />
+				</div>
 
+				<img
+					className="avatar"
+					alt=""
+					src={`http://localhost:9000/${props.image}`}
+				/>
 
-              
-      <img className="avatar"         alt=""               src={`http://localhost:9000/${props.image}`}/>
+				<div className="footSection">
+					<div className="likesShareDate">
+						<div className="likesShare">
+							<img className="likesIcon" alt="" src={likes_icon} />
+							<img className="shareIcon" alt="" src={share_icon} />
+						</div>
+						<h6 className="date"> {props.date} </h6>
+					</div>
 
+					<div>
+						<h6 className="likes"> {props.likes}likes </h6>
+					</div>
 
-      <div className="footSection">    
-          <div className="likesShareDate">
-          <div className="likesShare">
-            <img className="likesIcon"  alt=""               src={likes_icon} />
-            <img className="shareIcon"  alt=""               src={share_icon} />
-          </div>
-          <h6  className="date">        {props.date}         </h6>
-        </div>
-
-        <div>
-          <h6  className="likes">       {props.likes}likes     </h6>
-          
-        </div> 
-
-        <div>
-          <h3  className="description"> {props.description}  </h3>
-        </div> 
-      
-      
-      </div>
-    </div>
-    </div>
-  );
+					<div>
+						<h3 className="description"> {props.description} </h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
-
-
-
